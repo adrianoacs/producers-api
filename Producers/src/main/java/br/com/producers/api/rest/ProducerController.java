b9bf0e7ac5682b4a310fe0b91ea0373eca84c242
trendt000104 <asouza@trendoperadora.com.br>
2021-11-07 17:40:15 -0300

    inicio inclusão swagger

--- Producers/src/main/java/br/com/producers/api/rest/ProducerController.java
+++ Producers/src/main/java/br/com/producers/api/rest/ProducerController.java
@@ -2,11 +2,16 @@ package br.com.producers.api.rest;
 
 import br.com.producers.application.service.ProducerService;
 import br.com.producers.domain.valueobjects.WinnerIntervalsVO;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@OpenAPIDefinition(servers = {
+        @Server(url = "http://localhost:8090/")
+})
 public class ProducerController {
 
     private ProducerService producerService;
@@ -20,3 +25,7 @@ public class ProducerController {
         return ResponseEntity.ok(producerService.getWinners());
     }
 }
+
+
+
+
